52398e8b8af23f5fdefbba192228f103dc1c7ff0
Juan Manuel López <juan_manuel_lopez_1996@hotmail.com>
2025-02-10 23:01:28 -0300

    add zustand

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,7 +1,16 @@
-const HomePage = () => (
-    <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-);
+import { useCounterStore } from '@react-client-side-seed/stores/counter.store';
+
+const HomePage = () => {
+    const { count, increment } = useCounterStore();
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <div>{count}</div>
+            <button type="button" onClick={increment}>
+                increment
+            </button>
+        </div>
+    );
+};
 
 export default HomePage;
